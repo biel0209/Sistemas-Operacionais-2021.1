bb3f34f33fa0c6d0d11acfa0d2be4e74688f5432
biel0209 <52585328+biel0209@users.noreply.github.com>
2021-09-18 11:15:41 -0300

    atualizado

--- MemoryManagement.c
+++ MemoryManagement.c
@@ -3,6 +3,8 @@
 #define TAM 5
 #define TAM_2 10
 
+//Recebe como parametros um ponteiro para um vetor de inteiros e
+//seu tamanho a fim de realizar sua impressao.
 void imprimirVetor(int *v, int tam)
 {
     for(int i=1; i<=tam; i++){
@@ -10,6 +12,20 @@ void imprimirVetor(int *v, int tam)
     }
 }
 
+/*
+Na funcao principal, inicialmente eh alocado memoria para um
+ponteiro que aponta para um vetor de inteiros vet(1...n),
+onde n eh o seu tamanho. O vet eh entao preenchido com
+inteiros que vao de 1 a n e posteriormente eh realizada a
+chamada da funcao imprimirVetor, passando como parametro vet
+e seu tamanho. Posteriormente eh chamada a funcao realloc
+para realocar o dobro de espaco para vet, que passara a ter
+tamanho de 2*n. As novas posicoes de vet serao preenchidas e
+sera impresso atraves da chamada de imprimirVetor novamente,
+passando agora como parametro o seu novo tamanho. Por fim, eh
+utilizada a funcao free para liberar a memoria ocupada por
+vet.
+*/
 int main()
 {
     int *vet = malloc(TAM*sizeof(int));
