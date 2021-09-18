e8e1d98464cf5cfbaeaa1e5c073ebccfb9d0b3ff
biel0209 <52585328+biel0209@users.noreply.github.com>
2021-09-18 11:23:42 -0300

    Update Process3.c

--- Process3.c
+++ Process3.c
@@ -3,6 +3,10 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+/*Neste programa eh impresso o PID do processo, seguido pela
+chamada da funcao system, que tem como argumento o programa
+Process2. O intuito dessa chamada eh executar um segundo
+processo e depois retornar para o processo original.*/
 int main(void)
 {
     printf("PID do programa original: %d\n", getpid());
