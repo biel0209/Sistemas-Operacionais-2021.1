4ce1eb90e7059a3f63ed70a0969a9e4bfbf72675
biel0209 <52585328+biel0209@users.noreply.github.com>
2021-09-18 11:18:33 -0300

    atualizado

--- Process.c
+++ Process.c
@@ -2,6 +2,25 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+/*
+Neste programa eh utilizada as bibliotecas unistd.h e sys/types.h, 
+onde a primeira possibilita o uso das funcoes fork() e
+getpid(), e a segunda possibilita o uso do tipo pid_t,
+indicado para armazenar id de processos.
+Na funcao principal, eh chamada a funcao fork(), que tem o
+intuito de gerar um novo processo filho com as mesmas
+caracteristicas do processo original. O retorno de fork() eh
+o id do novo processo gerado, que sera armazenado na variavel
+num_pid. A seguir sera testado o id gerado atraves de
+condicoes (if): Se o id gerado for menor que zero, houve um erro na 
+duplicacao do processo; Se o id for igual a zero, significa que o 
+processo que esta atuando eh o filho e seu pid eh impresso.
+Se o id for diferente das condicoes acima, significa que o processo 
+que esta atuando eh o pai, ou seja, eh o processo original e seu pid eh 
+impresso. Ao fim dessas condicoes, o programa vai para o processo filho 
+e testa novamente todas as condicoes (por causa da chamada de fork(), 
+todo o processo se repete). So entao, o programa finaliza.
+*/
 int main()
 {
     pid_t num_pid = fork();
